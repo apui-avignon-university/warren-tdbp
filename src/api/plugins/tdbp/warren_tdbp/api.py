15ed34633ccbdc852aeff5da4bd14e9666819f4a
Quitterie Lucas <quitterielucas@outlook.fr>
2023-12-14 16:58:41 +0100

    ✨(indicators) implement active actions computations
    WIP.

---
+++ src/api/plugins/tdbp/warren_tdbp/api.py
@@ -0,0 +1,57 @@
+"""Warren API v1 tdbp router."""
+
+import logging
+from datetime import datetime
+from typing import Optional
+
+from fastapi import APIRouter, status
+from ralph.exceptions import HTTPException, LrsClientException
+
+from .indicators import ActiveActionsIndicator, ScoreIndicator
+
+router = APIRouter(
+    prefix="/tdbp",
+)
+
+logger = logging.getLogger(__name__)
+
+
+@router.get("/window")
+async def get_sliding_window(
+    until: datetime,
+    course_id: str,
+):
+    """Return course sliding window indicator."""
+    indicator = ActiveActionsIndicator(course_id=course_id, until=until)
+
+    logger.debug("Start computing 'window' indicator")
+    try:
+        results = indicator.compute()
+    except (KeyError, AttributeError, LrsClientException) as exception:
+        message = "An error occurred while computing window date range"
+        logger.exception("%s. Exception:", message)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=message
+        ) from exception
+
+    logger.debug("Finish computing 'window' indicator")
+    return results
+
+
+@router.get("/score")
+async def get_score(until: datetime, course_id: str, student_id: Optional[str] = None):
+    """Return student or cohort score."""
+    logger.debug("Start computing 'score' indicator")
+    indicator = ScoreIndicator(course_id=course_id, until=until, student_id=student_id)
+
+    try:
+        results = indicator.fetch()
+    except (KeyError, AttributeError, LrsClientException) as exception:
+        message = "An error occurred while computing student score(s)"
+        logger.exception("%s. Exception:", message)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=message
+        ) from exception
+
+    logger.debug("Finish computing 'score' indicator")
+    return results
